0be3a24b95ff9dcb1f760ceb111d77fc401e039f
Xinia <neko_1996@mail.ru>
2023-07-01 10:23:50 -0600

    Add options
    Added options according to feedback

--- Задание-2.js
+++ Задание-2.js
@@ -2,6 +2,7 @@
 Напишите функцию, которая принимает на входе любое число (но не больше 1 000), определяет, является ли оно простым, и выводит, 
 простое число или нет. Если введено больше 1 000, то выводится сообщение, что данные неверны. Обратите внимание на числа 0 и 1.*/
  
+// Вариант 1
 function searchPrime(a) {
     if (a > 1000) {
         alert( "Данные неверны" );
@@ -20,3 +21,28 @@ function searchPrime(a) {
 };
 
 alert(searchPrime(13));
+
+// Вариант 2
+function checkPrime(number) {
+    if (number > 1000) {
+        console.log( "Данные неверны" );
+        return;
+    } 
+    if (number === 0 || number === 1) {
+        console.log( "Введены 1 или 0, простыми числами не являются" );
+        return;
+    } 
+    let isPrime = true;
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            console.log("Вы ввели простое число");
+        } else {
+            console.log("Число не является простым");
+        }
+    }
